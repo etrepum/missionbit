af331631bac14d70f5d6e1b1480f7abcb78b9a25
Tyson Daugherty <Tyson@TYSONs-MacBook-Pro.local>
2013-08-12 13:23:04 -0700

    pledge values being captured

--- client/template_methods/donate.js
+++ client/template_methods/donate.js
@@ -5,4 +5,11 @@ Template.letsDonate.toDonate = function () {
 }
 
 
-
+Template.letsDonate.events({
+  'click #donate-pledge-submit': function (event) {
+    event.preventDefault();
+    var pledgeValue = $('input:radio[name=pledge]:checked').val();
+    // var pledgeValue = document.getElementById("pledge-form").value;
+    console.log(pledgeValue);
+  }
+})
