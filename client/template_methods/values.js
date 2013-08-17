5dedec963d937f9b54399a63d12976cf63d46d87
Tyson Daugherty <Tyson@TYSONs-MacBook-Pro.local>
2013-08-17 13:32:28 -0700

    newsletter signup put om values page

--- client/template_methods/values.js
+++ client/template_methods/values.js
@@ -4,3 +4,20 @@ Template.showValues.makeMeCare = function () {
   return iCare;
 }
 
+Template.showValues.events({
+  'keypress #values-input': function (event) {
+    mixpanel.track("Newsletter Email");
+    if (event.which == 13) {
+      event.preventDefault();
+      var thisEmail = document.getElementById("values-input").value;
+      console.log(thisEmail);
+      Newsletters.insert({
+        email: thisEmail
+      })
+      $('#values-input').remove();
+      $('#values-email-input').append('<input id="values-input" class="span11 values-email-input" type="email" placeholder="Enter Email Here" autofocus />');
+      $('#values-input-confirm-msg').append('<span id="values-email-confirm" class="span11"> You will receive newsletters via <span class="input-output"> '+ thisEmail +'</span></span>');
+    }
+  }
+})
+
